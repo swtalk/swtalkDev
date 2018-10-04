06ae6be057f3d6e53b857f5d84d330292267faf4
nhnent <sw.talk@nhnent.com>
2018-10-04 16:22:13 +0900

    Origin 변경테스트

--- accountbook/urls.py
+++ accountbook/urls.py
@@ -1,4 +1,4 @@
-"""accountbook URL Configuration
+"""accountbook URL Configuratio
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
